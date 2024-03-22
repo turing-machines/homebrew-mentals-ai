d16a8afa95853c41960b77970e69fde1665f885d
postrv <71533421+postrv@users.noreply.github.com>
2024-03-22 12:15:44 +0000

    Changing checksums to be generated programmatically too.

--- mentals-ai.rb
+++ mentals-ai.rb
@@ -1,12 +1,12 @@
 class MentalsAi < Formula
-  desc "Custom AI Agents built in Rust"
+  desc "The place where AI agents live, they are essentially small intelligent beings - mentals..."
   homepage "https://turingpi.com"
   if Hardware::CPU.intel?
     url "https://github.com/turing-machines/mentals-ai/releases/download/#{version}/mentals-ai-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "8c2b83d3841b27c2983a9aa79633488931c413c3c2375d7be98df2708da37ac4"
+    sha256 "generated-checksum-intel"
   elsif Hardware::CPU.arm?
     url "https://github.com/turing-machines/mentals-ai/releases/download/#{version}/mentals-ai-#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "17b0f7fe05f1824fac3df843e9bee8bc38adfb6f7611847e00dc7bbf5bb1587a"
+    sha256 "generated-checksum-arm"
   end
   license "MIT"
 
